3c57390c6a890d9f8b98f247d89e7031964076e2
jChavarria22 <55501652+jChavarria22@users.noreply.github.com>
2019-10-06 12:41:22 -0600

    Ventana de navegación para el administrador captura de error

--- app/src/main/java/com/example/fumigacionesmoncada/CitaFragment.java
+++ app/src/main/java/com/example/fumigacionesmoncada/CitaFragment.java
@@ -79,12 +79,18 @@ public class CitaFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+        try {
+            if (context instanceof OnFragmentInteractionListener) {
+                mListener = (OnFragmentInteractionListener) context;
+            } else {
+                throw new RuntimeException(context.toString()
+                        + " must implement OnFragmentInteractionListener");
+            }
+
+        }catch (Exception e ){
+
         }
+
     }
 
     @Override

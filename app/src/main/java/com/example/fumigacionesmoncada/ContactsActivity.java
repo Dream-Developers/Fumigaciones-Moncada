3f7410a4a97b300e22a046640e5d2a8ea3410bad
jacob-aguilar <55472724+jacob-aguilar@users.noreply.github.com>
2019-11-10 19:48:10 -0600

    Lista de Contactos

--- app/src/main/java/com/example/fumigacionesmoncada/ContactsActivity.java
+++ app/src/main/java/com/example/fumigacionesmoncada/ContactsActivity.java
@@ -1,14 +1,104 @@
 package com.example.fumigacionesmoncada;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.Item;
+import com.xwray.groupie.ViewHolder;
+
+import java.util.List;
 
 public class ContactsActivity extends AppCompatActivity {
+
+    private GroupAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        RecyclerView rv = findViewById(R.id.recycler);
+        adapter = new GroupAdapter();
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+
+
+        SearchUsers();
+
     }
+
+
+    /**
+     * METODO PARA IR A FIREBASE A BUSCAR LOS USUARIOS
+     * */
+    private void SearchUsers() {
+        FirebaseFirestore.getInstance().collection("/users")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                        if (e != null){
+                            Log.e("Teste", e.getMessage(), e);
+                            return;
+                        }
+
+                        List<DocumentSnapshot> docs = queryDocumentSnapshots.getDocuments();
+                        for (DocumentSnapshot doc: docs) {
+                            User user = doc.toObject(User.class);
+                            Log.d("Teste", user.getUsername());
+
+                            adapter.add(new UserItem(user));
+                        }
+                    }
+                });
+    }
+
+
+
+    /**
+     * METODO PARA AGREGAR LOS DATOS A CADA ITEM DE LA CLASE
+     * */
+    private class UserItem extends Item<ViewHolder> {
+
+        private final User user;
+
+        private UserItem(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public void bind(@NonNull ViewHolder viewHolder, int position) {
+            Log.d("Teste", position + "");
+            TextView txtUsername = viewHolder.itemView.findViewById(R.id.textView);
+            ImageView imgPhoto = viewHolder.itemView.findViewById(R.id.imageView);
+
+            txtUsername.setText(user.getUsername());
+
+            Picasso.get()
+                    .load(user.getProfileUrl())
+                    .into(imgPhoto);
+        }
+
+        @Override
+        public int getLayout() {
+            return R.layout.item_contactos;
+        }
+
+    }
+
 }

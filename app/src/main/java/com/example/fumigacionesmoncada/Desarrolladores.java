b712f4825c6f1ef0026ee0cc2d1bd038b3b159dd
Karen Eloisa Flores <kareneloisa_f@yahoo.com>
2020-03-18 13:05:43 -0600

    nuevas mejora

--- app/src/main/java/com/example/fumigacionesmoncada/Desarrolladores.java
+++ app/src/main/java/com/example/fumigacionesmoncada/Desarrolladores.java
@@ -15,5 +15,4 @@ public class Desarrolladores extends AppCompatActivity {
     }
 
 
-
 }
--- app/src/main/java/com/example/fumigacionesmoncada/ui/citas/Crear_Citas.java
+++ app/src/main/java/com/example/fumigacionesmoncada/ui/citas/Crear_Citas.java
@@ -1,19 +1,14 @@
 package com.example.fumigacionesmoncada.ui.citas;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -24,17 +19,17 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.fumigacionesmoncada.ClaseVolley;
 import com.example.fumigacionesmoncada.R;
-import com.example.fumigacionesmoncada.factura.crearFactura;
 import com.example.fumigacionesmoncada.ui.clientes.ClientesAdapter;
 import com.example.fumigacionesmoncada.ui.clientes.ClientesVO;
 
@@ -79,9 +74,6 @@ public class Crear_Citas extends AppCompatActivity   implements AdapterView.OnIt
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_citas);
-
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         fecha = findViewById(R.id.registro_Fecha);
         etHora = findViewById(R.id.registro_Hora);
         nombre = findViewById(R.id.registro_nombres);
@@ -105,7 +97,7 @@ public class Crear_Citas extends AppCompatActivity   implements AdapterView.OnIt
                 obtenerHora();
             }
         });
-
+        etHora.setEnabled(false);
         cargarPreferencias();
 
 
@@ -231,14 +223,6 @@ public class Crear_Citas extends AppCompatActivity   implements AdapterView.OnIt
 
         ClaseVolley.getIntanciaVolley(Crear_Citas.this).addToRequestQueue(jsonObjectRequest);
 
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==android.R.id.home){
-            finish();
-        }
-        return true;
     }
 
     private void obtenerHora() {

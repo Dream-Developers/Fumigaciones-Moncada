9abf716b541d43fe4038b3178a83ab65727d2a7b
jacob-aguilar <55472724+jacob-aguilar@users.noreply.github.com>
2020-06-17 02:20:24 -0600

    User information in header

--- app/src/main/java/com/example/fumigacionesmoncada/NavegacionAdministradorActivity.java
+++ app/src/main/java/com/example/fumigacionesmoncada/NavegacionAdministradorActivity.java
@@ -11,8 +11,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -29,9 +33,16 @@ import com.example.fumigacionesmoncada.notifications.Token;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -40,8 +51,14 @@ public class NavegacionAdministradorActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    StorageReference storageReference;
     ContentResolver resolver;
     String mUID;
+    TextView nameTv, emailTv;
+    ImageView avatarIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +69,18 @@ public class NavegacionAdministradorActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         System.out.println(firebaseAuth.getCurrentUser());
+        user = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+        storageReference = FirebaseStorage.getInstance().getReference("Images");
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        View navHeaderView = navigationView.getHeaderView(0);
+
+        avatarIv =  navHeaderView.findViewById(R.id.imageView_header);
+        nameTv =  navHeaderView.findViewById(R.id.name_header);
+        emailTv =  navHeaderView.findViewById(R.id.email_header);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -81,6 +107,36 @@ public class NavegacionAdministradorActivity extends AppCompatActivity {
         //checkUserStatus();
         //updateToken(FirebaseInstanceId.getInstance().getToken());
 
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    String name = ""+ ds.child("name").getValue();
+                    String email = ""+ ds.child("email").getValue();
+                    String image = ""+ ds.child("image").getValue();
+
+                    //Set datamain
+                    nameTv.setText(name);
+                    emailTv.setText(email);
+
+
+                    try {
+                        Picasso.get().load(image).into(avatarIv);
+                    }catch (Exception e){
+                        Picasso.get().load(R.drawable.fondo_chat4).into(avatarIv);
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     /**private void updateToken(String token) {
@@ -94,7 +150,7 @@ public class NavegacionAdministradorActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_admin, menu);
-        menu.findItem(R.id.action_search).setVisible(false);
+        //menu.findItem(R.id.action_search).setVisible(false);
         return true;
     }
 
@@ -109,6 +165,21 @@ public class NavegacionAdministradorActivity extends AppCompatActivity {
 
     public void red_admin(MenuItem item) {
         alertaCerrarSesionAdmin();
+    }
+
+    public void mision_vision_admin(MenuItem item) {
+        Intent myIntent1 = new Intent(this, VisionMision.class);
+        startActivity(myIntent1);
+    }
+
+    public void desarrolladores_admin(MenuItem item) {
+        Intent myIntent2 = new Intent(this, Desarrolladores.class);
+        startActivity(myIntent2);
+    }
+
+    public void ayuda_admin(MenuItem item) {
+        Intent myIntent3 = new Intent(this, ContenedorAdminActivity.class);
+        startActivity(myIntent3);
     }
 
     public Cursor obtenerRegistrosFecha(){
